0cad58c1483bed667b1445c3558bd99f8bbc86a4
John Murphy <john.murphy2@ucdconnect.ie>
2019-11-13 18:28:20 +0000

    add camera to add photo

--- app/src/main/java/com/morgan/declan/samplelogin/ui/additem/AddItemFragment.java
+++ app/src/main/java/com/morgan/declan/samplelogin/ui/additem/AddItemFragment.java
@@ -1,12 +1,14 @@
 package com.morgan.declan.samplelogin.ui.additem;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -22,6 +25,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -37,13 +41,17 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.morgan.declan.samplelogin.ItemArrayAdapter;
 import com.morgan.declan.samplelogin.Post;
 import com.morgan.declan.samplelogin.R;
 import com.morgan.declan.samplelogin.Upload;
 import com.morgan.declan.samplelogin.ui.home.HomeViewModel;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -52,6 +60,9 @@ public class AddItemFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private StorageReference mStorageReference;
     private DatabaseReference mDatabase;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private String currentPhotoPath;
 
     private EditText et_title;
     private EditText et_desc;
@@ -60,6 +71,7 @@ public class AddItemFragment extends Fragment {
 
     private Button buttonChoose;
     private FloatingActionButton buttonUpload;
+    private ImageButton buttonTakePhoto;
     //uri to store file
     private Uri filePath;
     private View rootView;
@@ -76,6 +88,7 @@ public class AddItemFragment extends Fragment {
         final View root = inflater.inflate(R.layout.fragment_additem, container, false);
 
         buttonChoose = root.findViewById(R.id.choose_image);
+        buttonTakePhoto = root.findViewById(R.id.take_photo);
         buttonUpload = root.findViewById(R.id.floatingAddPostButton);
 
         rootView = root;
@@ -110,6 +123,12 @@ public class AddItemFragment extends Fragment {
                 uploadFile(root);
             }
         });
+        buttonTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         return root;
     }
@@ -138,13 +157,51 @@ public class AddItemFragment extends Fragment {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), 234);
     }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 234 && data != null && data.getData() != null) {
             filePath = data.getData();
-
             Uri uri = data.getData();
 
             try {
@@ -154,9 +211,24 @@ public class AddItemFragment extends Fragment {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+        }
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+//            filePath = data.getData();
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            ImageView imageView = rootView.findViewById(R.id.image_preview);
+//            imageView.setImageBitmap(imageBitmap);
+            galleryAddPic();
         }
     }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getContext().sendBroadcast(mediaScanIntent);
+    }
+
 
     public String getFileExtension(Uri uri) {
         ContentResolver cR = getActivity().getContentResolver();
@@ -165,8 +237,6 @@ public class AddItemFragment extends Fragment {
     }
 
     private void uploadFile(View root) {
-
-
 
         EditText nameText = root.findViewById(R.id.addItemName);
         String name = nameText.getText().toString();
